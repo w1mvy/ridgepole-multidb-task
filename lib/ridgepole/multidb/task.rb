0d606eb6067cc22bd69d8116772bf610fd5f6956
w1mvy <w1mvypine@gmail.com>
2015-09-11 14:19:09 +0900

    Fix load file name

--- lib/ridgepole/multidb/task.rb
+++ lib/ridgepole/multidb/task.rb
@@ -3,7 +3,7 @@ if defined? Rails
   module Ridgepole
     class Railtie < Rails::Railtie
       rake_tasks do
-        load "ridgepole/multidb/task/db.rake"
+        load "ridgepole/multidb/task/schema.rake"
       end
     end
   end
